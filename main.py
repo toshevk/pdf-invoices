96300451a83a9c4ec8f0e46bdb7528eb1a475835
Krste <toshev.krste@yahoo.com>
2023-08-22 20:16:57 +0200

    Add the invoice with a table of items purchased

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    # Read excel files in data frames
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
-
     # Format name
     filename = Path(filepath).stem
     # invoice_number, invoice_date = filename.split("-")
@@ -25,5 +22,30 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}", ln=1)
+
+    # Read excel files in data frames
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add column headers
+    column_headers = df.columns
+    column_headers = [item.replace("_", " ").title() for item in column_headers]
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(50, 50, 50)
+    pdf.cell(w=30, h=8, txt=column_headers[0], border=1)
+    pdf.cell(w=70, h=8, txt=column_headers[1], border=1)
+    pdf.cell(w=35, h=8, txt=column_headers[2], border=1)
+    pdf.cell(w=30, h=8, txt=column_headers[3], border=1)
+    pdf.cell(w=30, h=8, txt=column_headers[4], border=1, ln=1)
+
+    # Populate table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(70, 70, 70)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=35, h=8, txt=str(row['amount_purchased']), align="R", border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), align="R", border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), align="R", border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
